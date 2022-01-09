ddb9e297b1e2406a2e4ef16722cb67a3d741f890
whara123 <whara123@gmail.com>
2022-01-10 03:16:36 +0900

    :sparkles: Add GameOver condition

--- js/app.js
+++ js/app.js
@@ -1,24 +1,50 @@
 document.addEventListener('DOMContentLoaded',()=>{
   const chiken = document.querySelector(".chiken");
+  const topWalls = document.querySelectorAll(".top-wall");
+  const downWalls = document.querySelectorAll(".down-wall");
 
-  let chikenLeft = 220;
-  let chikenBottom = 100;
-  let gravity = 2;
-  let jumpPower = 500;
 
+  let chikenLeft = 100;
+  let chikenBottom = 250;
+
+  let gravity = 0.3;
+  let jumpPower = 3;
+  
   function gameStart(){
-    chikenBottom += gravity;
-    chiken.style.transform = `translate(${chikenLeft}px,${chikenBottom}px)`;
-    console.log(chiken.style.transform);
+    chikenBottom += jumpPower;
+    jumpPower -= gravity;
+    chiken.style.left = `${chikenLeft}px`;
+    chiken.style.bottom = `${chikenBottom}px`;
+    
+    topWalls.forEach((topwall)=>{
+      if(chiken.offsetLeft<=(topwall.offsetLeft+topwall.offsetWidth) &&
+      (chiken.offsetLeft+chiken.offsetWidth)>=(topwall.offsetLeft+topwall.offsetWidth) &&
+      chiken.offsetTop<=(topwall.offsetTop+topwall.offsetHeight) &&
+      (chiken.offsetTop+chiken.offsetHeight)>=topwall.offsetTop){
+        console.log("게임 오버");
+      }
+    });
+
+    downWalls.forEach((downwall)=>{
+      if(chiken.offsetLeft<=(downwall.offsetLeft+downwall.offsetWidth) &&
+      (chiken.offsetLeft+chiken.offsetWidth)>=(downwall.offsetLeft+downwall.offsetWidth) &&
+      chiken.offsetTop<=(downwall.offsetTop+downwall.offsetHeight) &&
+      (chiken.offsetTop+chiken.offsetHeight)>=downwall.offsetTop){
+        console.log("게임 오버");
+      }
+    });
+
+    
+    
   }
-  let timer = setInterval(gameStart);
+  let timer = setInterval(gameStart,15);
+  
+
+
+
 
   function jump(){
-    // chikenBottom -= gravity*250;
-    let nowPos = chikenBottom;
-    chikenBottom -= jumpPower;
-    console.log(nowPos-=jumpPower);
-    // chiken.style.transform = `translate(${chikenLeft}px,${chikenBottom}px)`;
+    jumpPower = 7;
   }
 
   document.addEventListener('click',()=>{
