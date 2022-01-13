29ab2d5cc18e5b3a24f152d561a3838e671078b4
whara123 <whara123@gmail.com>
2022-01-14 03:54:42 +0900

    :sparkles: Add gameover

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded',()=>{
+
   const chiken = document.querySelector(".chiken");
   const topWalls = document.querySelectorAll(".top-wall");
   const downWalls = document.querySelectorAll(".down-wall");
-
+  const scoreBoard = document.querySelector(".score");
 
   let chikenLeft = 100;
   let chikenBottom = 250;
 
   let gravity = 0.3;
   let jumpPower = 3;
-  let randomPos=0;
+  
+  let Score=0;
+  let game = true;
+  scoreBoard.innerText = `Score : ${Score}`
+
+  
+  let bestScore=0;
 
   function gameStart(){
     chikenBottom += jumpPower;
@@ -19,21 +26,32 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     topWalls.forEach((topwall)=>{
       if(chiken.offsetLeft<=(topwall.offsetLeft+topwall.offsetWidth) &&
-      (chiken.offsetLeft+chiken.offsetWidth)>=(topwall.offsetLeft+topwall.offsetWidth) &&
+      (chiken.offsetLeft+chiken.offsetWidth)>=topwall.offsetLeft &&
       chiken.offsetTop<=(topwall.offsetTop+topwall.offsetHeight) &&
       (chiken.offsetTop+chiken.offsetHeight)>=topwall.offsetTop){
-        console.log("게임 오버");
+        gameOver();
       }
     });
 
     downWalls.forEach((downwall)=>{
       if(chiken.offsetLeft<=(downwall.offsetLeft+downwall.offsetWidth) &&
-      (chiken.offsetLeft+chiken.offsetWidth)>=(downwall.offsetLeft+downwall.offsetWidth) &&
+      (chiken.offsetLeft+chiken.offsetWidth)>=downwall.offsetLeft &&
       chiken.offsetTop<=(downwall.offsetTop+downwall.offsetHeight) &&
       (chiken.offsetTop+chiken.offsetHeight)>=downwall.offsetTop){
-        console.log("게임 오버");
+        gameOver();
       }
     });
+
+    topWalls.forEach((topWall)=>{
+      if((chiken.offsetLeft+chiken.offsetWidth)>(topWall.offsetLeft+topWall.offsetWidth)){
+        if(topWall.classList.value === "top-wall check"){
+          Score++;
+          scoreBoard.innerText = `Score : ${Score}`
+          topWall.classList.remove("check");
+        }
+      }
+    });
+
 
   }
   let timer = setInterval(gameStart,15);
@@ -41,20 +59,37 @@ document.addEventListener('DOMContentLoaded',()=>{
   topWalls.forEach((topWall,idx)=>{
     topWall.addEventListener("animationiteration",()=>{
       randomPos = Math.floor(Math.random()*160-40)+40;
-     
+      
       topWall.style.top=`-${randomPos}px`;
       downWalls[idx].style.top = `${(-randomPos)+530}px`;
+      if(topWall.classList.value === "top-wall"){
+        topWall.classList.add("check");
+      }
     })
   })
 
-
-
   function jump(){
-    jumpPower = 7;
+    jumpPower = 6.5;
   }
 
   document.addEventListener('click',()=>{
-    jump();
+    if(game){
+      jump();
+    }
   });
+
+  function gameOver(){
+    
+    game=false;
+
+    topWalls.forEach((topwall)=>{
+      topwall.style.animationPlayState = 'paused';
+    })
+    downWalls.forEach((downwall)=>{
+      downwall.style.animationPlayState = 'paused';
+    })
+    document.querySelector(".dimd").classList.add("on");
+    
+  }
 
 })
